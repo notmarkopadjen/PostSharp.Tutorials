cff9ee463b3eb4f252aeb2e478645beb6b3aa585
notmarkopadjen <p@djen.email>
2020-07-25 19:57:04 +0200

    NLog multi-target implemented

--- Logging/PostSharp.Tutorials.Logging/Program.cs
+++ Logging/PostSharp.Tutorials.Logging/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using PostSharp.Tutorials.Logging.BusinessLogic;
+﻿using PostSharp.Tutorials.Logging.BusinessLogic;
 using PostSharp.Patterns.Diagnostics;
-using PostSharp.Patterns.Diagnostics.Backends.Console;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using PostSharp.Patterns.Diagnostics.Backends.NLog;
+using NLog.Layouts;
 
 namespace PostSharp.Tutorials.Logging
 {
@@ -9,11 +12,31 @@ namespace PostSharp.Tutorials.Logging
     {
         static void Main(string[] args)
         {
-            // Configure PostSharp Logging to write to the console.
-            var backend = new ConsoleLoggingBackend();
-            backend.Options.Theme = ConsoleThemes.Dark;
-            backend.Options.IncludeExceptionDetails = false;
-            LoggingServices.DefaultBackend = backend;
+            var nlogConfig = new LoggingConfiguration();
+
+            var traceTarget = new FileTarget("file")
+            {
+                FileName = "trace.log",
+                KeepFileOpen = true,
+                ConcurrentWrites = false,
+                Header = new SimpleLayout("Time|Level|Class|Method|Info")
+            };
+            nlogConfig.AddTarget(traceTarget);
+            nlogConfig.LoggingRules.Add(new LoggingRule("*", NLog.LogLevel.Trace, traceTarget));
+
+            var warnTarget = new FileTarget("file")
+            {
+                FileName = "warn.log",
+                KeepFileOpen = true,
+                ConcurrentWrites = false,
+                Header = new SimpleLayout("Time|Level|Class|Method|Info")
+            };
+            nlogConfig.AddTarget(warnTarget);
+            nlogConfig.LoggingRules.Add(new LoggingRule("*", NLog.LogLevel.Warn, warnTarget));
+
+            LogManager.EnableLogging();
+
+            LoggingServices.DefaultBackend = new NLogLoggingBackend(new LogFactory(nlogConfig));
 
             RequestProcessor requestProcessor = new RequestProcessor();
             requestProcessor.ProcessRequests("requestsQueue");
